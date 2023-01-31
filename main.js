f34cb4295c7319c8a7b0f31b47c66f5da136c876
Phạm Hoàng An <83413846+AnPH13@users.noreply.github.com>
2023-01-31 09:35:07 +0700

    update using shake.js

--- main.js
+++ main.js
@@ -31,34 +31,25 @@ window.onload = function () {
                 .then(response => {
                     // (optional) Do something after API prompt dismissed.
                     if (response == "granted") {
-                        var sensitivity = 20;
+                        var myShakeEvent = new Shake({
+                            threshold: 15
+                        });
 
-                        // Position variables
-                        var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
+                        // start listening to device motion
+                        myShakeEvent.start();
 
-                        // Listen to motion events and update the position
-                        window.addEventListener('devicemotion', function (e) {
-                            x1 = e.accelerationIncludingGravity.x;
-                            y1 = e.accelerationIncludingGravity.y;
-                            z1 = e.accelerationIncludingGravity.z;
-                        }, false);
+                        // register a shake event
+                        window.addEventListener('shake', shakeEventDidOccur, false);
 
-                        // Periodically check the position and fire
-                        // if the change is greater than the sensitivity
-                        setInterval(function () {
-                            var change = Math.abs(x1 - x2 + y1 - y2 + z1 - z2);
+                        //shake event callback
+                        function shakeEventDidOccur() {
 
-                            if (change > sensitivity) {
-                                anph.style.display = "block";
-                                anphText.style.display = "none";
-                                startAnimate();
-                            }
+                            //put your own code here etc.
+                            anph.style.display = "block";
+                            anphText.style.display = "none";
+                            startAnimate();
+                        }
 
-                            // Update new position
-                            x2 = x1;
-                            y2 = y1;
-                            z2 = z1;
-                        }, 150);
                     }
                 })
                 .catch(console.error)
