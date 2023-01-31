721cbd69231d875a74432f0ddc6ddd2fa8e15ebb
Phạm Hoàng An <83413846+AnPH13@users.noreply.github.com>
2023-01-31 09:51:19 +0700

    update

--- main1.js
+++ main1.js
@@ -1,21 +1,75 @@
 function getMobileOperatingSystem() {
+    var anph = document.getElementById("anph13-activate");
+    var anphText = document.getElementById("anph13-h1");
+
+    if (location.protocol != "https:") {
+        location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+    }
+
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    // Windows Phone must come first because its UA also contains "Android"
-    if (/windows phone/i.test(userAgent)) {
-        alert( "Windows Phone");
-    }
-
-    if (/android/i.test(userAgent)) {
-        alert( "Android");
-    }
 
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        alert( "iOS");
+        permission()
+        return 0;
     }
 
-    alert( "unknown");
+    var myShakeEvent = new Shake({
+        threshold: 15
+    });
+
+    // start listening to device motion
+    myShakeEvent.start();
+
+    // register a shake event
+    window.addEventListener('shake', shakeEventDidOccur, false);
+
+    //shake event callback
+    function shakeEventDidOccur() {
+
+        //put your own code here etc.
+        anph.style.display = "block";
+        anphText.style.display = "none";
+        startAnimate();
+    }
+
+    return 0;
 }
+
+
+function permission() {
+    if (typeof (DeviceMotionEvent) !== "undefined" && typeof (DeviceMotionEvent.requestPermission) === "function") {
+        // (optional) Do something before API request prompt.
+        DeviceMotionEvent.requestPermission()
+            .then(response => {
+                // (optional) Do something after API prompt dismissed.
+                if (response == "granted") {
+                    var myShakeEvent = new Shake({
+                        threshold: 15
+                    });
+
+                    // start listening to device motion
+                    myShakeEvent.start();
+
+                    // register a shake event
+                    window.addEventListener('shake', shakeEventDidOccur, false);
+
+                    //shake event callback
+                    function shakeEventDidOccur() {
+
+                        //put your own code here etc.
+                        anph.style.display = "block";
+                        anphText.style.display = "none";
+                        startAnimate();
+                    }
+
+                }
+            })
+            .catch(console.error)
+    } else {
+        alert("DeviceMotionEvent is not defined");
+    }
+}
+
 
 window.onload = getMobileOperatingSystem()
