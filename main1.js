d89304f5faa5444ab2a151a4859262f60f03de2a
Phạm Hoàng An <83413846+AnPH13@users.noreply.github.com>
2023-01-31 10:14:19 +0700

    update fix

--- main1.js
+++ main1.js
@@ -2,41 +2,48 @@ function getMobileOperatingSystem() {
     var anph = document.getElementById("anph13-activate");
     var anphText = document.getElementById("anph13-h1");
 
-    
+    if (location.protocol != "https:") {
+        location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+    }
 
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        
-        if(permission()){
-            if (location.protocol != "https:") {
-                location.href = "https:" + window.location.href.substring(window.location.protocol.length);
-            }
-            var myShakeEvent = new Shake({
-                threshold: 15
-            });
-        
-            // start listening to device motion
-            myShakeEvent.start();
-        
-            // register a shake event
-            window.addEventListener('shake', shakeEventDidOccur, false);
-        
-            //shake event callback
-            function shakeEventDidOccur() {
-        
-                //put your own code here etc.
-                anph.style.display = "block";
-                anphText.style.display = "none";
-                startAnimate();
-            }
+        alert("IOS")
+        if (typeof (DeviceMotionEvent) !== "undefined" && typeof (DeviceMotionEvent.requestPermission) === "function") {
+            // (optional) Do something before API request prompt.
+            DeviceMotionEvent.requestPermission()
+                .then(response => {
+                    // (optional) Do something after API prompt dismissed.
+                    if (response == "granted") {
+                        alert("IOS pass")
+                        var myShakeEvent = new Shake({
+                            threshold: 15
+                        });
+                    
+                        // start listening to device motion
+                        myShakeEvent.start();
+                    
+                        // register a shake event
+                        window.addEventListener('shake', shakeEventDidOccur, false);
+                    
+                        //shake event callback
+                        function shakeEventDidOccur() {
+                    
+                            //put your own code here etc.
+                            anph.style.display = "block";
+                            anphText.style.display = "none";
+                            startAnimate();
+                            alert("on shake");
+                        }
+                    }
+                })
+                .catch(console.error)
         }
-        return 0;
+        return;
     }
-    if (location.protocol != "https:") {
-        location.href = "https:" + window.location.href.substring(window.location.protocol.length);
-    }
+    
     var myShakeEvent = new Shake({
         threshold: 15
     });
@@ -77,9 +84,7 @@ function permission() {
                 }
             })
             .catch(console.error)
-    } else {
-       return false;
-    }
+    } 
 }
 
 
